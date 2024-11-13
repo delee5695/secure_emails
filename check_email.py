4d0524b123d672f4527cb85ee9c07c58677380e2
zcoakley <157329265+zcoakley@users.noreply.github.com>
2024-11-13 17:01:29 -0500

    Take message out of email

--- check_email.py
+++ check_email.py
@@ -6,13 +6,16 @@ import os
 # Access the app password
 load_dotenv()
 app_pass = os.getenv('APP_PASS')  # Retrieve the API key
+my_email = os.getenv('MY_EMAIL')
 if not app_pass:
     raise EnvironmentError("APP_PASS not found in .env file or environment variables")
+if not my_email:
+    raise EnvironmentError("MY_EMAIL not found in .env file or environment variables")
 
 
 # Gmail IMAP settings
 IMAP_SERVER = 'imap.gmail.com'
-EMAIL = 'your_email'
+EMAIL = my_email
 PASSWORD = app_pass
 
 def read_emails():
@@ -27,22 +30,22 @@ def read_emails():
     status, messages = mail.search(None, 'ALL')
     email_ids = messages[0].split()
 
-    for e_id in email_ids[-1:]:  # Fetch the last 5 emails
+    for e_id in email_ids[-1:]:  # Fetch the last 1 emails
         status, msg_data = mail.fetch(e_id, '(RFC822)')
         for response_part in msg_data:
             if isinstance(response_part, tuple):
                 msg = email.message_from_bytes(response_part[1])
-                print("From:", msg["from"])
-                print("Subject:", msg["subject"])
+
                 if msg.is_multipart():
                     for part in msg.walk():
                         if part.get_content_type() == "text/plain":
-                            print(part.get_payload(decode=True).decode())
+                            message = part.get_payload(decode=True).decode()
                 else:
-                    print(msg.get_payload(decode=True).decode())
-                # print("-" * 50)
+                    message = msg.get_payload(decode=True).decode()
 
     mail.logout()
 
+    return message
+
 if __name__ == "__main__":
-    read_emails()
+    print(read_emails().split()[0])
